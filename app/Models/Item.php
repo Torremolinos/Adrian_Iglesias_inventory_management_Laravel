1dfd19d81112cb6d40b368c5973c73a5b26a4435
Torremolinos <adrian_2054@hotmail.com>
2024-02-25 23:36:17 +0100

    Creados los modelos

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+    public function box(){
+        return $this->belongsTo(Box::class);
+    }
+    public function loan(){
+        return $this->belongsTo(Loan::class);
+    }
 }
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
+
     use HasFactory;
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function items(){
+        return $this->hasOne(Item::class);
+    }
 }
