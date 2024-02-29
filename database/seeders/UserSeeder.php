9b73c9309a83594a035384981219e2286358dae5
Torremolinos <adrian_2054@hotmail.com>
2024-02-29 09:43:38 +0100

    finish the seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,35 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Usuario 1',
+                'email' => 'u1@example.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // Puedes añadir más usuarios aquí
+        ]);
+        DB::table('users')->insert([
+            [
+                'name' => 'Usuario 2',
+                'email' => 'u2@example.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // Puedes añadir más usuarios aquí
+        ]);
+        DB::table('users')->insert([
+            [
+                'name' => 'Usuario 3',
+                'email' => 'u3@example.com',
+                'password' => Hash::make('password'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            // Puedes añadir más usuarios aquí
+        ]);
     }
 }
